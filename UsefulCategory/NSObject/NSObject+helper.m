b6da2ebf17dd0d645e45298c20418e61e15e23de
LuochuanAD <luochuanpersonal@163.com>
2018-04-13 11:23:46 +0800

    交换

---
+++ UsefulCategory/NSObject/NSObject+helper.m
@@ -0,0 +1,68 @@
+//
+//  NSObject+helper.m
+//  UsefulClass
+//
+//  Created by care on 2018/4/13.
+//  Copyright © 2018年 luochuan. All rights reserved.
+//
+
+#import "NSObject+helper.h"
+#import <objc/runtime.h>
+
+@implementation NSObject (helper)
+
++ (void)swizzleClassMethod:(SEL)origSelector withMethod:(SEL)newSelector
+{
+    Class cls = [self class];
+    
+    Method originalMethod = class_getClassMethod(cls, origSelector);
+    Method swizzledMethod = class_getClassMethod(cls, newSelector);
+    
+    Class metacls = objc_getMetaClass(NSStringFromClass(cls).UTF8String);
+    if (class_addMethod(metacls,
+                        origSelector,
+                        method_getImplementation(swizzledMethod),
+                        method_getTypeEncoding(swizzledMethod)) ) {
+        /* swizzing super class method, added if not exist */
+        class_replaceMethod(metacls,
+                            newSelector,
+                            method_getImplementation(originalMethod),
+                            method_getTypeEncoding(originalMethod));
+        
+    } else {
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
+}
+
+- (void)swizzleInstanceMethod:(SEL)origSelector withMethod:(SEL)newSelector
+{
+    Class cls = [self class];
+    /* if current class not exist selector, then get super*/
+    Method originalMethod = class_getInstanceMethod(cls, origSelector);
+    Method swizzledMethod = class_getInstanceMethod(cls, newSelector);
+    /* add selector if not exist, implement append with method */
+    if (class_addMethod(cls,
+                        origSelector,
+                        method_getImplementation(swizzledMethod),
+                        method_getTypeEncoding(swizzledMethod)) ) {
+        /* replace class instance method, added if selector not exist */
+        /* for class cluster , it always add new selector here */
+        class_replaceMethod(cls,
+                            newSelector,
+                            method_getImplementation(originalMethod),
+                            method_getTypeEncoding(originalMethod));
+        
+    } else {
+        /* swizzleMethod maybe belong to super */
+        class_replaceMethod(cls,
+                            newSelector,
+                            class_replaceMethod(cls,
+                                                origSelector,
+                                                method_getImplementation(swizzledMethod),
+                                                method_getTypeEncoding(swizzledMethod)),
+                            method_getTypeEncoding(originalMethod));
+    }
+}
+
+
+@end
