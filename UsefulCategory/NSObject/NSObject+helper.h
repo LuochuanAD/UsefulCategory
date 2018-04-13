324517dedd086a5b743e65f77484fa493e9caca1
LuochuanAD <luochuanpersonal@163.com>
2018-04-13 11:24:07 +0800

    交换

---
+++ UsefulCategory/NSObject/NSObject+helper.h
@@ -0,0 +1,19 @@
+//
+//  NSObject+helper.h
+//  UsefulClass
+//
+//  Created by care on 2018/4/13.
+//  Copyright © 2018年 luochuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (helper)
+
+//交换类方法
++ (void)swizzleClassMethod:(SEL)origSelector withMethod:(SEL)newSelector;
+
+//交换实例方法
+- (void)swizzleInstanceMethod:(SEL)origSelector withMethod:(SEL)newSelector;
+
+@end
